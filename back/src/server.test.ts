947b9468dd2f63fee8d739277e249b25c46c6ec5
anton <antoniogallegobernal179@gmail.com>
2024-01-26 17:56:16 -0500

    Add React to Project and update all endpoints: /a -> /api/a

---
+++ back/src/server.test.ts
@@ -0,0 +1,241 @@
+import axios, { AxiosError, AxiosResponse } from "axios";
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
+import { Author, Book, NewAuthor, NewBook } from "./server.js";
+
+let port: number = 3000;
+let host: string = "localhost";
+let protocol: string = "http";
+let baseUrl: string = `${protocol}://${host}:${port}`;
+
+axios.defaults.baseURL = baseUrl;
+
+sqlite3.verbose();
+let db: any;
+const initDatabase = async () => {
+    if (!db) {
+        db = await open<sqlite3.Database, sqlite3.Statement>({
+            filename: "database.db",
+            driver: sqlite3.Database
+        });
+    }
+}
+
+let initialData: { books: [Book], authors: [Author] };
+
+beforeEach(async () => {
+    await initDatabase();
+    initialData = { books: await db.all("SELECT * FROM books"), authors: await db.all("SELECT * FROM authors") };
+})
+
+test("GET /api/books returns list of all books", async () => {
+    let response = await axios.get("/api/books");
+    expect(response.data).toEqual(initialData.books);
+})
+
+test("GET /api/books/validID returns correct book", async () => {
+    let bookID: number = 1;
+    let response: AxiosResponse<Book> = await axios.get(`/api/books/${bookID}`);
+    expect(response.data).toEqual(initialData.books.filter((book) => book.id == bookID)[0]);
+}) 
+
+test("Query strings work correctly", async () => {
+    try {
+        let response: AxiosResponse<Book[]> = await axios.get("/api/books?genre='fantasy'&&author_id=2&&pub_year=2009");
+        let book: Book = (await axios.get("/api/books?title='Alas Negras'")).data[0];
+        expect(response.data.length).toEqual(1);
+        expect(response.data[0]).toEqual(book);
+        let queriedAuthor: Author = (await axios.get("/api/authors?name='Laura Gallego'")).data[0];
+        expect(queriedAuthor).toEqual(initialData.authors.filter((author) => author.name == 'Laura Gallego')[0])
+        await axios.get("/api/books?name='Alas Negras'"); // Test invalid query parameter: name
+    } catch (error) {
+        let errorObj = error as AxiosError;
+        if (errorObj.response === undefined) {
+            throw Error("Server never sent response");
+        }
+        let { response } = errorObj;
+        expect(response.status).toEqual(400);
+    }
+})
+
+test("GET /api/books/invalidID returns error", async () => {
+    let invalidBookID: number = 222;
+    try {
+        await axios.get(`/api/books/${invalidBookID}`);
+    } catch (error) {
+        let errorObj = error as AxiosError;
+        if (errorObj.response === undefined) {
+            throw Error("Server never sent response");
+        }
+        let { response } = errorObj;
+        expect(response.status).toEqual(404);
+        expect(response.data).toEqual({ error: `No book was found with ID: ${invalidBookID}` });
+    }
+})
+
+test("GET /api/authors returns list of all authors", async () => {
+    let response: AxiosResponse<Author[]> = await axios.get("/api/authors");
+    expect(response.data).toEqual(initialData.authors);
+})
+
+test("GET /api/authors/validID returns correct author", async () => {
+    let authorID: number = 1;
+    let response: AxiosResponse<Author> = await axios.get(`/api/authors/${authorID}`);
+    expect(response.data).toEqual(initialData.authors.filter((author) => author.id == authorID)[0]);
+}) 
+
+test("GET /api/authors/invalidID returns error", async () => {
+    let invalidAuthorID: number = 177;
+    try {
+        await axios.get(`/api/authors/${invalidAuthorID}`);
+    } catch (error) {
+        let errorObj = error as AxiosError;
+        if (errorObj.response === undefined) {
+            throw Error("Server never sent response");
+        }
+        let { response } = errorObj;
+        expect(response.status).toEqual(404);
+        expect(response.data).toEqual({ error: `No author was found with ID: ${invalidAuthorID}` });
+    }
+}) 
+
+test("POST /api/authors works as expected and returns new author with automatically assigned ID", async () => {
+    let newAuthor: NewAuthor = { name: "Tonke Dragt", bio: "Dutch writer and illustrator" }
+    let response: AxiosResponse<Author> = await axios.post("/api/authors", newAuthor);
+    let confirmation: AxiosResponse<Author> = await axios.get(`/api/authors/${response.data.id}`);
+    expect(response.status).toEqual(201);
+    expect(response.data).toEqual(confirmation.data);
+})
+
+test("POST /api/authors invalid object shape returns error", async () => {
+    let invalidAuthor: { nimm: string, bio: string } = { nimm: "This isn't right", bio: "This bio is ok" }
+    try {
+        await axios.post("/api/authors", invalidAuthor);
+    } catch (error) {
+        let errorObj = error as AxiosError;
+        if (errorObj.response === undefined) {
+            throw Error("Server never sent response");
+        }
+        let { response } = errorObj;
+        expect(response.status).toEqual(400);
+    }
+})
+
+test("POST /api/books valid authorID works as expected and returns new book with automatically assigned ID", async () => {
+    let newBook: NewBook = { author_id: 2, title: "Finis Mundi", pub_year: 1999, genre: "fiction" };
+    let response: AxiosResponse<Book> = await axios.post("/api/books", newBook);
+    let confirmation: AxiosResponse<Book> = await axios.get(`/api/books/${response.data.id}`);
+    expect(response.status).toEqual(201);
+    expect(response.data).toEqual(confirmation.data);
+}) 
+
+test("POST /api/books invalid authorID returns error", async () => {
+    let invalidBook: NewBook = { author_id: 179, title: "Invalid Author Book", pub_year: 1987, genre: "mystery" }
+    try {
+        await axios.post("/api/books", invalidBook);
+    } catch (error) {
+        let errorObj = error as AxiosError;
+        if (errorObj.response === undefined) {
+            throw Error("Server never sent response");
+        }
+        let { response } = errorObj;
+        expect(response.status).toEqual(400);
+    }
+}) 
+
+test("POST /api/books invalid object shape returns error", async () => {
+    let invalidBook: { author_id: number, pubbb_year: number, title: string, genre: string } = { author_id: 2, pubbb_year: 1977, title: "Invalid book", genre: "biography" }
+    try {
+        await axios.post("/api/books", invalidBook);
+    } catch (error) {
+        let errorObj = error as AxiosError;
+        if (errorObj.response === undefined) {
+            throw Error("Server never sent response");
+        }
+        let { response } = errorObj;
+        expect(response.status).toEqual(400);
+    }
+})
+
+test("POST /api/books invalid book genre returns error", async () => {
+    let invalidBook: { author_id: number, pub_year: number, title: string, genre: string } = { author_id: 1, pub_year: 1977, title: "Invalid genre", genre: "idk" }
+    try {
+        await axios.post("/api/books", invalidBook);
+    } catch (error) {
+        let errorObj = error as AxiosError;
+        if (errorObj.response === undefined) {
+            throw Error("Server never sent response");
+        }
+        let { response } = errorObj;
+        expect(response.status).toEqual(400);
+    }
+}) 
+
+test("DELETE /api/authors/validID with no books in db works as expected", async () => {
+    await axios.post("/api/authors", { name: "Deleted", bio: "She will be deleted" });
+    let authorToDelete: Author = (await axios.get("/api/authors?name='Deleted'")).data[0];
+    let deleteResponse: AxiosResponse<null> = await axios.delete(`/api/authors/${authorToDelete.id}`);
+    try {
+        await axios.get(`/api/authors/${authorToDelete.id}`);
+    } catch (error) {
+        let errorObj = error as AxiosError;
+        if (errorObj.response === undefined) {
+            throw Error("Server never sent response");
+        }
+        let { response } = errorObj;
+        expect(deleteResponse.status).toEqual(204);
+        expect(response.status).toEqual(404);
+    }
+}) 
+
+test("DELETE /api/authors/validID with books still in db returns error", async () => {
+    let authorPostResponse: AxiosResponse<Author> = await axios.post("/api/authors", { name: "Book Expert", bio: "She has published many books" });
+    let newAuthorID: number = authorPostResponse.data.id;
+    await axios.post("/api/books", { author_id: newAuthorID, title: "How I became an expert author", pub_year: 2020, genre: "biography" });
+    try {
+        await axios.delete(`/api/authors/${newAuthorID}`);
+    } catch (error) {
+        let errorObj = error as AxiosError;
+        if (errorObj.response === undefined) {
+            throw Error("Server never sent response");
+        }
+        let { response } = errorObj;
+        expect(response.status).toEqual(403);
+        expect(response.data).toEqual({ error: "Author still has books associated, please delete those first" });
+    }
+}) 
+
+test("DELETE /api/authors/invalidID returns error", async () => {
+    try {
+        await axios.delete("/api/authors/234");
+    } catch (error) {
+        let errorObj = error as AxiosError;
+        if (errorObj.response === undefined) {
+            throw Error("Server never sent response");
+        }
+        let { response } = errorObj;
+        expect(response.status).toEqual(404);
+    }
+}) 
+
+test("DELETE /api/books/validID works as expected", async () => {
+    await axios.post("/api/authors", { name: "Newbie", bio: "She has not published any books yet" });
+    let newAuthorID: number = (await axios.get("/api/authors?name='Newbie'")).data[0].id;
+    let postResponse: AxiosResponse<Book> = await axios.post("/api/books", { author_id: newAuthorID, title: "My First Book!", pub_year: 2023, genre: "biography" })
+    let deleteResponse: AxiosResponse<null> = await axios.delete(`/api/books/${postResponse.data.id}`);
+    expect(postResponse.status).toEqual(201);
+    expect(deleteResponse.status).toEqual(204);
+}) 
+
+test("DELETE /api/books/invalidID returns error", async () => {
+    try {
+        await axios.delete("/api/books/180");
+    } catch (error) {
+        let errorObj = error as AxiosError;
+        if (errorObj.response === undefined) {
+            throw Error("Server never sent response");
+        }
+        let { response } = errorObj;
+        expect(response.status).toEqual(404);
+    }
+}) 
---
+++ back/src/server.ts
@@ -0,0 +1,267 @@
+import express, { Request, Response } from "express";
+import sqlite3 from "sqlite3";
+import { Database, ISqlite, Statement, open } from "sqlite";
+import * as url from "url";
+import { z } from "zod";
+
+let app = express();
+app.use(express.json());
+
+let __dirname: string = url.fileURLToPath(new URL("..", import.meta.url));
+let dbfile: string = `${__dirname}database.db`;
+let db: Database = await open({
+    filename: dbfile,
+    driver: sqlite3.Database,
+});
+await db.get("PRAGMA foreign_keys = ON");
+
+export interface Error {
+    error: string | string[];
+}
+
+type Genre = "romance" | "mystery" | "fiction" | "fantasy" | "adventure" | "action" | "science fiction" | "biography";
+
+export interface Book {
+    id: number,
+    author_id: number,
+    title: string,
+    pub_year: number,
+    genre: Genre
+}
+
+export interface NewBook {
+    author_id: number,
+    title: string,
+    pub_year: number,
+    genre: Genre
+}
+
+export interface Author {
+    id: number,
+    name: string,
+    bio: string
+}
+
+export interface NewAuthor {
+    name: string,
+    bio: string
+}
+
+type BookResponse = Response<Book | Error>;
+type AuthorResponse = Response<Author | Error>;
+type BooksResponse = Response<Book[] | Error>;
+type AuthorsResponse = Response<Author[] | Error>;
+
+interface BooksGetRequest extends Request {
+    query: {
+        id: string,
+        author_id: string,
+        title: string,
+        pub_year: string,
+        genre: Genre
+    },
+    params: {
+        id: string
+    }
+}
+
+interface AuthorsGetRequest extends Request {
+    query: {
+        id: string,
+        name: string,
+        bio: string
+    },
+    params: {
+        id: string
+    }
+}
+
+interface AuthorRequest extends Request {
+    body: NewAuthor
+}
+
+interface BookRequest extends Request {
+    body: NewBook
+}
+
+interface DeleteRequest extends Request {
+    params: {
+        id: string
+    }
+}
+
+let genres = ["romance", "mystery", "fiction", "fantasy", "adventure", "action", "science fiction", "biography"] as const;
+
+let newAuthorBodySchema = z.object({
+    name: z.string(),
+    bio: z.string()
+})
+
+let newBookBodySchema = z.object({
+    author_id: z.number(),
+    title: z.string(),
+    pub_year: z.number(),
+    genre: z.enum(genres)
+})
+
+function parseError(zodError: z.ZodError): string[] {
+    let { formErrors, fieldErrors } = zodError.flatten();
+    return [
+        ...formErrors,
+        ...Object.entries(fieldErrors).map(
+            ([property, message]) => `"${property}": ${message}`
+        ),
+    ];
+}
+
+function addQueryStringSupport(req: Request, res: Response, tablename: string): string {
+    let queryParams: string[] = [];
+    let queryString: string = `SELECT * FROM ${tablename} `;
+    let queryObj: any = req.query;
+    for (const parameter in req.query) {
+        queryParams.push(parameter);
+    }
+    for (const i in queryParams) {
+        if (queryString == `SELECT * FROM ${tablename} `) {
+            queryString += `WHERE ${queryParams[i]}=${queryObj[queryParams[i]]} `;
+        } else {
+            queryString += `AND ${queryParams[i]}=${queryObj[queryParams[i]]} `;
+        }
+    }
+    return queryString;
+}
+
+app.get("/api/books", async (req: BooksGetRequest, res: BooksResponse) => {
+    let result: Book[];
+    try {
+        result = await db.all(addQueryStringSupport(req, res, "books"));
+    } catch (err) {
+        let error = err as Object;
+        return res.status(400).json({ error: error.toString() });
+    }
+    return res.status(200).json(result);
+})
+
+app.get("/api/books/:id", async (req, res: BookResponse) => {
+    let result: Book[] | undefined;
+    try {
+        result = await db.all<Book[]>(`SELECT * FROM books WHERE ID='${req.params.id}'`);
+        if (result === undefined) {
+            return res.status(404).json({ error: `No book was found with ID: ${req.params.id}` });
+        }
+    } catch (err) {
+        let error = err as Object;
+        return res.status(400).json({ error: error.toString() });
+    }
+    return res.status(200).json(result[0]);
+})
+
+app.get("/api/authors", async (req: AuthorsGetRequest, res: AuthorsResponse) => {
+    let result: Author[];
+    try {
+        result = await db.all(addQueryStringSupport(req, res, "authors"));
+    } catch (error) {
+        let err = error as Object;
+        return res.status(400).json({ error: err.toString() })
+    }
+    return res.status(200).json(result);
+})
+
+app.get("/api/authors/:id", async (req, res: AuthorResponse) => {
+    let result: Author[] | undefined;
+    try {
+        result = await db.all(`SELECT * FROM authors WHERE ID='${req.params.id}'`);
+        if (result === undefined) {
+            return res.status(404).json({ error: `No author was found with ID: ${req.params.id}` });
+        }
+    } catch (error) {
+        let err = error as Object;
+        return res.status(400).json({ error: err.toString() });
+    }
+    return res.status(200).json(result[0]);
+})
+
+app.post("/api/books", async (req: BookRequest, res: BookResponse) => {
+    let newBook: NewBook;
+    let insertedBook: Book;
+    try {
+        let parseResult = newBookBodySchema.safeParse(req.body);
+        if (!parseResult.success) {
+            throw parseError(parseResult.error);
+        }
+        let authorCheck: Author[] = await db.all(`SELECT * FROM AUTHORS WHERE ID=${req.body.author_id}`);
+        if (authorCheck.length == 0) {
+            throw "The author associated with author_id specified does not exist";
+        }
+        newBook = req.body;
+        let statement: Statement = await db.prepare("INSERT INTO BOOKS(id, author_id, title, pub_year, genre) VALUES(?, ?, ?, ?, ?)");
+        await statement.bind([null, newBook.author_id, newBook.title, newBook.pub_year, newBook.genre]);
+        let result: ISqlite.RunResult = await statement.run();
+        let newID: number|undefined = result.lastID;
+        insertedBook = (await db.all(`SELECT * FROM BOOKS WHERE ID=${newID}`))[0];
+    } catch (error) {
+        let err = error as Object;
+        return res.status(400).json({ error: err.toString() });
+    }
+    return res.status(201).json(insertedBook);
+});
+
+app.post("/api/authors", async (req: AuthorRequest, res: AuthorResponse) => {
+    let newAuthor: NewAuthor;
+    let insertedAuthor: Author;
+    try {
+        let parseResult = newAuthorBodySchema.safeParse(req.body);
+        if (!parseResult.success) {
+            throw parseError(parseResult.error);
+        }
+        newAuthor = req.body;
+        let statement: Statement = await db.prepare("INSERT INTO authors(id, name, bio) VALUES(?, ?, ?)");
+        await statement.bind([null, newAuthor.name, newAuthor.bio]);
+        let result: ISqlite.RunResult = await statement.run();
+        let newID: number|undefined = result.lastID;
+        insertedAuthor = (await db.all(`SELECT * FROM AUTHORS WHERE ID=${newID}`))[0];
+    } catch (error) {
+        let err = error as Object;
+        return res.status(400).json({ error: err.toString() });
+    }
+    return res.status(201).json(insertedAuthor);
+})
+
+app.delete("/api/authors/:id", async (req: DeleteRequest, res) => {
+    try {
+        let exists: Author[] = await db.all(`SELECT * FROM AUTHORS WHERE ID=${req.params.id}`);
+        if (exists.length == 0) {
+            return res.sendStatus(404)
+        }
+        let hasBooks: Book[] = await db.all(`SELECT * FROM BOOKS WHERE AUTHOR_ID=${req.params.id}`);
+        if (hasBooks.length != 0) {
+            return res.status(403).json({ error: "Author still has books associated, please delete those first" });
+        }
+        await db.all(`DELETE FROM AUTHORS WHERE ID=${req.params.id}`);
+    } catch (error) {
+        let err = error as Object;
+        return res.status(400).json({ error: err.toString() });
+    }
+    return res.sendStatus(204);
+})
+
+app.delete("/api/books/:id", async (req: DeleteRequest, res) => {
+    try {
+        let exists: Book[] = await db.all(`SELECT * FROM BOOKS WHERE ID=${req.params.id}`);
+        if (exists.length == 0) {
+            return res.sendStatus(404)
+        }
+        await db.all(`DELETE FROM BOOKS WHERE ID=${req.params.id}`);
+    } catch (error) {
+        let err = error as Object;
+        return res.status(400).json({ error: err.toString() });
+    }
+    return res.sendStatus(204);
+})
+
+let port: number = 3000;
+let host: string = "localhost";
+let protocol: string = "http";
+app.listen(port, host, () => {
+    console.log(`${protocol}://${host}:${port}`);
+});
